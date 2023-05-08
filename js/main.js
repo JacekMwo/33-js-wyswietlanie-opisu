047d31d5879e3c4bc25f168a08df6983f65929fa
JacekMwo <jckbanach@gmail.com>
2023-05-08 12:31:40 +0200

    33-js-wyswietlanie-opisu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let nazwa = document.getElementById('opis');
+let guzik = document.querySelector('button');
+guzik.onclick = function () {
+    let nazwa = document.querySelector('opis');
+    guzik.textContent += ' Nowy iPhone 13 został wyposażony w 6,1-calowy wyświetlacz OLED. Biorąc pod uwagę sam ekran smartfona iPhone 13, wymiary matrycy w porównaniu z modelem Pro są identyczne. Różnica tkwi jedynie w częstotliwości odświeżania ekranu.'
+}
